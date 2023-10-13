842addcd376cfa06d09804f2d3c55532bb6ddba7
caiodavi442@gmail.com <caiodavi442@gmail.com>
2023-10-13 11:28:24 -0300

    media semente protótipo 1

--- GA_Proteinas-main/MediaSemente.py
+++ GA_Proteinas-main/MediaSemente.py
@@ -5,15 +5,23 @@ path = "./melhores"
 
 arquivos = os.listdir(path)
 lista = []
+i = 0
+soma = 0
 
 for arquivo in arquivos:
     with open(os.path.join(path, arquivo), 'r') as file:
-        conteudo = file.read()
-        lista.append(conteudo)
-        
+        for linha in file:
+            linha = linha.replace("\n", " ")
+            p = re.compile(r"(\d+\.\d+)\,")
+            aux = p.findall(linha)
+            if aux != []:
+                lista.append(aux.__getitem__(0))
 
-for i in lista:
-    aux = i
-    lista2 = aux.split("(")
-    
-print(lista2)
+while i != lista.__len__():
+    if  i%11 != 0:
+        soma += float(lista[i])
+    else:
+        media = soma/11
+        soma = 0
+        soma += float(lista[i])
+    i += 1
