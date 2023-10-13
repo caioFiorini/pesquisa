ccf10f17e8ab04209bb6db26ce11c86d69b5394b
caiodavi442@gmail.com <caiodavi442@gmail.com>
2023-10-13 12:01:41 -0300

    Media Semente Funcional protótipo 1

--- GA_Proteinas-main/MediaSemente.py
+++ GA_Proteinas-main/MediaSemente.py
@@ -1,5 +1,6 @@
 import os
 import re
+import matplotlib.pyplot as plt
 
 path = "./melhores"
 
@@ -7,6 +8,7 @@ arquivos = os.listdir(path)
 lista = []
 i = 0
 soma = 0
+media = []
 
 for arquivo in arquivos:
     with open(os.path.join(path, arquivo), 'r') as file:
@@ -18,10 +20,30 @@ for arquivo in arquivos:
                 lista.append(aux.__getitem__(0))
 
 while i != lista.__len__():
-    if  i%11 != 0:
-        soma += float(lista[i])
-    else:
-        media = soma/11
-        soma = 0
-        soma += float(lista[i])
+    soma += float(lista[i])
     i += 1
+    if i>0 and i%11 == 0:
+         # cada posição da lista é 1 semente;
+        media.append(soma/11)
+        soma = 0
+
+# Criar o gráfico de linha
+plt.figure(figsize=(6, 6))  # Tamanho da figura (opcional)
+
+plt.plot(arquivos, media, marker='o', linestyle='-', color='b', label='Média')
+
+# Definir rótulos e título
+plt.xlabel('Sementes')
+plt.ylabel('Média Sementes')
+plt.title('Média de cada semente')
+plt.grid(True)
+
+# Adicionar rótulos aos pontos
+for i, rate in enumerate(media):
+    plt.text(arquivos[i], rate, f'{rate:.2f}', ha='center', va='bottom')
+
+# Adicionar legenda
+plt.legend()
+
+# Exibir o gráfico
+plt.show()
