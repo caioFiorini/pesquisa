51258dbbdc2b50dc35b095e9b251eeb11c1cc8ca
VIctor <colen.victor@icloud.com>
2023-10-26 16:38:54 -0300

    refactor: rename AlgoritmoGeneticoMultiObjectivo to main.py

--- GA_Proteinas-main/UnusedAlgorithmFunctions.py
+++ GA_Proteinas-main/UnusedAlgorithmFunctions.py
@@ -1,8 +1,9 @@
 from deap import tools
 
+
 class UnusedAlgorithmFunctions:
     def selElitistAndTournament(individuals, k, frac_elitist, tournsize):
-        """_summary_ 
+        """_summary_
             Faz a seleção dos melhores e aplica o torneio.
         Args:
             individuals (list): Uma lista de individuos.
@@ -13,9 +14,10 @@ class UnusedAlgorithmFunctions:
         Returns:
             _type_: retorna uma lista concatenada com a seleção dos melhores e a seleção do torneio.
         """
-        return tools.selBest(individuals, int(k*frac_elitist)) + tools.selTournament(individuals, int(k*(1-frac_elitist)), tournsize=tournsize)
-    
-    
+        return tools.selBest(individuals, int(k * frac_elitist)) + tools.selTournament(
+            individuals, int(k * (1 - frac_elitist)), tournsize=tournsize
+        )
+
     def get_best_attributes_from_individual(population):
         """_summary_ Retorna uma lista com as características do melhor indivíduo.
 
@@ -28,15 +30,14 @@ class UnusedAlgorithmFunctions:
         best_attributes = []
         fitness = 0
         for individual in population:
-            if(fitness < individual.fitness.values):
+            if fitness < individual.fitness.values:
                 best_attributes = individual
                 fitness = individual.fitness.values
 
         # manda o indivíduo para receber somente as características.
         best_attributes = get_attributes_of_individual(best_attributes)
         return best_attributes
-    
-    
+
     def InicializaPopulacao(pop):
         """_summary_ Incia a população
 
@@ -47,11 +48,10 @@ class UnusedAlgorithmFunctions:
             seed = random.randrange(1, 290)
             contador = 0
             for i in range(0, INDIVIDUAL_SIZE):
-
                 valor = random.randint(0, 1)
-                if (seed == contador):
+                if seed == contador:
                     break
 
-                if (valor == 1):
+                if valor == 1:
                     p[i] = valor
                     contador = contador + 1
