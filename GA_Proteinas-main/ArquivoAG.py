3b627fbee6e0b9f00aa1bdaa5d9bcb96569847e6
Caio Fiorini <caiodavi442@gmail.com>
2024-02-27 17:16:18 -0300

    cm4_fw

--- GA_Proteinas-main/ArquivoAG.py
+++ GA_Proteinas-main/ArquivoAG.py
@@ -30,20 +30,6 @@ class Algoritmo_Genetico:
         
         return self.AG
     
-    def setup_creator(self):
-        # O creator cria uma nova classe com o nome passado no parâmetro
-        # Em termos mais simples, essa linha de código cria uma nova classe de aptidão chamada FitnessMulti que tem dois componentes.
-        # O primeiro componente é positivo e o segundo componente é negativo. A biblioteca Deap irá minimizar o segundo componente
-        # da aptidão, o que significa maximizar o primeiro componente da aptidão.
-        creator.create("FitnessMulti", base.Fitness, weights=(1.0, -1.0))
-
-        # array.array -> é usado para criar um arranjo de tipos específicos.
-        # no caso o Type code é i, logo ele cria um arranjo de inteiros
-        # Ele cria um arranjo com os elementos do fitnes.
-        creator.create(
-            "Individual", array.array, typecode="i", fitness=creator.FitnessMulti
-        )
-    
     def count_individuals_relative_to_parent_average(population: list) -> (int, int):  # type: ignore
         """_summary_ tem a finalidade de contar quantos indivíduos na população atual são considerados
         "piores" ou "melhores" em relação à sua aptidão em comparação com a média da aptidão de seus pais.
--- GA_Proteinas-main/mainTeste.py
+++ GA_Proteinas-main/mainTeste.py
@@ -12,7 +12,7 @@ from deap import base, creator, tools
 
 # Local Imports
 from MultiObjectiveGeneticAlgorithm import MultiObjectiveGeneticAlgorithm
-from MOGAToolbox import MOGAToolbox
+from MOGAToolbox import MOGAToolbox as mt
 from FileManager import FileManager
 
 # ====== MARK: Defining paths and file names ======
@@ -51,66 +51,39 @@ SAMPLE_COUNT = 490
 TAMANHO_TRANSFORMADA = 10  # TODO: rename when find out what this is
 INDIVIDUAL_SIZE = 104
 
-PROTEIN_CLASSES_LIST = [
-    "Hidrolases",
-    "Isomerases",
-    "Liases",
-    "Ligases",
-    "Oxidoredutases",
-    "Transferases",
-]
 
-protein_matrix = []
-external_protein_matrix = []
-
-
-def setup_creator():
-    # O creator cria uma nova classe com o nome passado no parâmetro
-    # Em termos mais simples, essa linha de código cria uma nova classe de aptidão chamada FitnessMulti que tem dois componentes.
-    # O primeiro componente é positivo e o segundo componente é negativo. A biblioteca Deap irá minimizar o segundo componente
-    # da aptidão, o que significa maximizar o primeiro componente da aptidão.
-    creator.create("FitnessMulti", base.Fitness, weights=(1.0, -1.0))
-
-    # array.array -> é usado para criar um arranjo de tipos específicos.
-    # no caso o Type code é i, logo ele cria um arranjo de inteiros
-    # Ele cria um arranjo com os elementos do fitnes.
-    creator.create(
-        "Individual", array.array, typecode="i", fitness=creator.FitnessMulti
-    )
+# def load_proteins(base_file_path: str):
+#     index = 0
+#     for class_number, protein_class in enumerate(PROTEIN_CLASSES_LIST):
+#         # Dentro de cada pasta de classe, tem um arquivo .txt com o nome da classe
+#         # e os arquivos .csv que devem ser lidos
+#         protein_list = FileManager.get_text_file_contents(base_file_path, protein_class)
+#         for protein in protein_list:
+#             with open(
+#                 os.path.join(
+#                     base_file_path, protein_class, protein.rstrip("\n").rstrip("\r")
+#                 ),
+#                 "r",
+#             ) as csvfile:
+#                 protein_reader = csv.reader(csvfile, delimiter=";")
+#                 # Turning the protein_reader into a list helps later on when we want to use random access (check if we need it though).
+#                 protein_matrix.insert(index, list(protein_reader))
+#                 index = index + 1
 
 
-def load_proteins(base_file_path: str):
-    index = 0
-    for class_number, protein_class in enumerate(PROTEIN_CLASSES_LIST):
-        # Dentro de cada pasta de classe, tem um arquivo .txt com o nome da classe
-        # e os arquivos .csv que devem ser lidos
-        protein_list = FileManager.get_text_file_contents(base_file_path, protein_class)
-        for protein in protein_list:
-            with open(
-                os.path.join(
-                    base_file_path, protein_class, protein.rstrip("\n").rstrip("\r")
-                ),
-                "r",
-            ) as csvfile:
-                protein_reader = csv.reader(csvfile, delimiter=";")
-                # Turning the protein_reader into a list helps later on when we want to use random access (check if we need it though).
-                protein_matrix.insert(index, list(protein_reader))
-                index = index + 1
+# def load_external_DB_proteins(base_file_path: str) -> None:
+#     with open(
+#         os.path.join(
+#             base_file_path, EXTERNAL_DATABASE_FILE_NAME.rstrip("\n").rstrip("\r")
+#         ),
+#         "r",
+#     ) as csvfile:
+#         protein_reader = csv.reader(csvfile, delimiter=";")
+#         # why insert at 0? SUS!
+#         external_protein_matrix.insert(0, list(protein_reader))
 
 
-def load_external_DB_proteins(base_file_path: str) -> None:
-    with open(
-        os.path.join(
-            base_file_path, EXTERNAL_DATABASE_FILE_NAME.rstrip("\n").rstrip("\r")
-        ),
-        "r",
-    ) as csvfile:
-        protein_reader = csv.reader(csvfile, delimiter=";")
-        # why insert at 0? SUS!
-        external_protein_matrix.insert(0, list(protein_reader))
-
-
-def count_individuals_relative_to_parent_average(population: list) -> (int, int): # type: ignore
+def count_individuals_relative_to_parent_average(population: list) -> (int, int): #type: ignore
     """_summary_ tem a finalidade de contar quantos indivíduos na população atual são considerados
     "piores" ou "melhores" em relação à sua aptidão em comparação com a média da aptidão de seus pais.
 
@@ -169,18 +142,15 @@ def get_duplicate_individuals_count(population: list) -> int:
 def main():
     start_time = time.time()
 
-    setup_creator()
+    mt.setup_creator()
     evolution_toolbox = MOGAToolbox(
         INDIVIDUAL_SIZE,
         MUTATION_RATE,
         CLASSIFIER_FILE_NAME,
         CLASSIFIER_PATH,
         SAMPLE_COUNT,
-        PROTEIN_CLASSES_LIST,
         TAMANHO_TRANSFORMADA,
         DATABASE_PATH,
-        protein_matrix,
-        external_protein_matrix,
     ).toolbox
 
     hall_of_fame = tools.HallOfFame(HALL_OF_FAME_SIZE)
@@ -188,12 +158,6 @@ def main():
     # gera uma semente aleatória
     # random.seed(sys.argv[1])
     random.seed(1)
-
-    print("Loading protein DB...")
-    load_proteins(DATABASE_PATH)
-
-    print("Loading external protein DB...")
-    load_external_DB_proteins(EXTERNAL_DATABASE_PATH)
 
     # inicializa uma lista com os indivíduos da população
     population = evolution_toolbox.population(n=POPULATION_SIZE)
