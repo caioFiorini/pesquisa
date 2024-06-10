7aff3e1a22603fbd857c0d64c54fe776cb7aeafb
caiodavi442@gmail.com <caiodavi442@gmail.com>
2024-06-10 06:11:41 -0300

    melhoria no teste.sh

--- GA_Proteinas-main/teste.sh
+++ GA_Proteinas-main/teste.sh
@@ -8,13 +8,13 @@ current_out_folder=$experimentos_out_folder
 seed=(9529851295 45243)
 teste=0
 # min max iterador <- O experimento deve seguir essa ideia.
-Population=(16 20 1) 
-Generations=(16 20 ) 
-CrossOverFactor=(0.6 0.9)
+Population=(16 20 4) 
+Generations=(16 20 4) 
+CrossOverFactor=(0.6 0.9 0.3)
 TournamentSize=2
-MutationRate=(0.2 0.4)
+MutationRate=(0.2 0.4 0.2)
 ElitismFactor=1
-QuantidadedeExperimentos=0
+declare -g QuantidadedeExperimentos=0
 experimento_contador=1
 quantidade_semente=${#seed[@]}
 flag="true" 
@@ -24,48 +24,50 @@ contador_geracao_arquivo=0
 if [ -d "$current_out_folder" ]; then
     echo "Experiment has already been done"
 else
-    # echo "Beginning run"
-    # mkdir -p "$current_out_folder"  # This will create all necessary directories
+    echo "Beginning run"
+    mkdir -p "$current_out_folder"  # This will create all necessary directories
 
 
 
     for i in "${seed[@]}";
     do 
-        for j in $(seq ${Population[0]} ${Population[2]} $(("${Population[1]}"-1)))
+        for j in $(seq ${Population[0]} ${Population[2]} ${Population[1]})
         do
-            for k in $(seq ${Generations[0]} ${Generations[2]} $(("${Generations[1]}"-1)) );
+            for k in $(seq ${Generations[0]} ${Generations[2]} ${Generations[1]});
             do
-                for l in  $(seq ${CrossOverFactor[0]} ${CrossOverFactor[2]} $(("${CrossOverFactor[1]}"-0.1 | bc)));
+                for l in  $(seq ${CrossOverFactor[0]} ${CrossOverFactor[2]} ${CrossOverFactor[1]});
                 do
-                    for m in $(seq ${MutationRate[0]} ${MutationRate[2]} $(("${MutationRate[1]}"-0.1 | bc)));
+                    for m in $(seq ${MutationRate[0]} ${MutationRate[2]} ${MutationRate[1]});
                     do 
-                        QuantidadedeExperimentos = QuantidadeExperimentos + 1;
+                        # echo "$i" "$j" "$k" "$l" "${TournamentSize}" "$m" "${ElitismFactor}" "${contador_geracao_arquivo}" "$experiment_dir" 
+                        ((QuantidadedeExperimentos+=1));
                     done
                 done
             done
         done
     done
 
-    echo "A quantidade de experimetos é: ""${QuantidadeExperimentos}"
+    echo "A quantidade de experimetos é: "$QuantidadedeExperimentos
     read -p "digite s para sim e n para não (minusculo)" input
     if [ "$input" == "n" ];then
         exit 0
     fi
-
+   
     for i in "${seed[@]}";
     do 
-        for (( j = Population[0]; j < Population[1]; j = j + Population[2]));
+        for j in $(seq ${Population[0]} ${Population[2]} ${Population[1]})
         do
-            for (( k = Generations[0]; k < Generations[1]; k = k + Generations[2]));
+            for k in $(seq ${Generations[0]} ${Generations[2]} ${Generations[1]});
             do
-                for (( l = CrossOverFactor[0]; l < CrossOverFactor[1]; l = l + CrossOverFactor[2]));
+                for l in  $(seq ${CrossOverFactor[0]} ${CrossOverFactor[2]} ${CrossOverFactor[1]});
                 do
-                    for (( m = MutationRate[0]; m < MutationRate[1]; m = m + MutationRate[2]));
+                    for m in $(seq ${MutationRate[0]} ${MutationRate[2]} ${MutationRate[1]});
                     do 
                         contador_geracao_arquivo=$((contador_geracao_arquivo+1))
                         experiment_dir="${current_out_folder}experiment_${contador_geracao_arquivo}/"
-                        echo "$i" "$j" "$k" "$l" "${TournamentSize}" "$m" "${ElitismFactor}" "${contador_geracao_arquivo}" "$experiment_dir" "${experiment_dir}${contador_geracao_arquivo}"
-                        # python3 mainTeste.py "$i" "$j" "$k" "$l" "${TournamentSize}" "$m" "${ElitismFactor}" "${contador_geracao_arquivo}" "$experiment_dir" "${experiment_dir}${contador_geracao_arquivo}" 
+                        # echo "$i" "$j" "$k" "$l" "${TournamentSize}" "$m" "${ElitismFactor}" "${contador_geracao_arquivo}" "$experiment_dir" "${experiment_dir}${contador_geracao_arquivo}"
+                        mkdir -p "$experiment_dir"
+                        python3 mainTeste.py "$i" "$j" "$k" "$l" "${TournamentSize}" "$m" "${ElitismFactor}" "${contador_geracao_arquivo}" "$experiment_dir" > "${experiment_dir}$i.txt" 
                     done
                 done
             done
